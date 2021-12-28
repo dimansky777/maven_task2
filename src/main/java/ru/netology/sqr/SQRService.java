8b652b92d97cb65f9eaebbc87288d88fc300304f
Dmitriy Tikhonin <dimansky777@gmail.com>
2021-12-28 12:36:05 +0300

    files updated

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -3,14 +3,14 @@ package ru.netology.sqr;
 public class SQRService {
 
     public int countSqrt(int range1, int range2) {
-        int counter;
+        int counter = 0;
         for (int i = 10; i <= 99; i++) {
             if (i * i >= range1 | i * i <= range2) {
-                counter = +1;
-                System.out.println(counter);
+                counter = counter +1;
+                //  System.out.println(counter);
+                return counter;
             }
         }
-        return 0;
+        return counter;
     }
-
 }
