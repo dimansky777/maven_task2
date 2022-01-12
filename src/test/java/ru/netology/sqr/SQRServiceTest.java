1ec99308e4ff52af711b4e42b9969968b676e676
Dmitriy Tikhonin <dimansky777@gmail.com>
2022-01-12 15:17:44 +0300

    New test for SQR service

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -16,4 +16,26 @@ class SQRServiceTest {
         int actual = sqrService.countSqrt(range1,range2);
         assertEquals(expected, actual);
     }
+
+    @Test
+    void shouldSqrtCount_1() {
+        SQRService sqrService = new SQRService();
+        int range1 = 300;
+        int range2 = 400;
+        // int counter = 0;
+        int expected = 3;
+        int actual = sqrService.countSqrt(range1,range2);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldSqrtCount_2() {
+        SQRService sqrService = new SQRService();
+        int range1 = 400;
+        int range2 = 900;
+        // int counter = 0;
+        int expected = 11;
+        int actual = sqrService.countSqrt(range1,range2);
+        assertEquals(expected, actual);
+    }
 }
